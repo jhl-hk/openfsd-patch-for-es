f618d82d78628a02f3a643b1effffd61df21a318
Jianyue Hugo Liang <valor@jianyuelab.org>
2025-06-19 21:45:13 +0900

    Update config.h

--- src/config.h
+++ src/config.h
@@ -2,4 +2,4 @@
  Copyright (c) 2025 Misaka Nnnnq. All rights reserved.
  */
 
-#define  TARGET_JWT_URL "https://example.com/api/fsd\0"
+#define  TARGET_JWT_URL "https://airwaysn.org/api/v1/public/auth\0"
